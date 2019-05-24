3e46b96d49038f739d5e5262e49dd716317cbfe2
Juan Manuel Duran <juanmanuel03@gmail.com>
2019-05-23 20:02:58 -0500

    Clase #2

---
+++ src/clase_02.java
@@ -0,0 +1,22 @@
+public class clase_02 {
+    int num;
+    char letra;
+    double real;
+    String hola;
+
+    public void imprimir(){
+        System.out.println("Clase #2");
+        System.out.println("Mi nombre ->");
+    }
+       int resultado(int edad){
+        int reult =0;
+        return edad+5;
+       }
+
+       public static void main(Sring[] arg){
+        int temp = 0;
+
+       }
+
+
+}
